66414a0e0f4b9ffa94e8ba898c91d2ebb25b2a24
vadym_fomin <vadym.fomin@nure.ua>
2017-02-13 13:55:34 +0200

    Fix bags

--- src/main/java/ua/nure/parsers/Marshaller.java
+++ src/main/java/ua/nure/parsers/Marshaller.java
@@ -2,7 +2,9 @@ package ua.nure.parsers;
 
 import ua.nure.entities.Teams;
 
+import java.io.File;
+
 public interface Marshaller {
 
-    void marshall(Teams teams);
+    void marshall(Teams teams, File file);
 }

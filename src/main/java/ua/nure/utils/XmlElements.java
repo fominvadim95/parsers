a857d6b581ccb311492fcdd83ccacec3cf8444f4
vadym_fomin <vadym.fomin@nure.ua>
2017-02-13 21:12:45 +0200

    Add utils

---
+++ src/main/java/ua/nure/utils/XmlElements.java
@@ -0,0 +1,42 @@
+package ua.nure.utils;
+
+
+public class XmlElements {
+
+    public static final String TEAM = "team";
+
+    public static final String ID = "id";
+
+    public static final String GENERAL = "general";
+
+    public static final String PLAYERS = "players";
+
+    public static final String PLAYER = "player";
+
+    public static final String NAME = "name";
+
+    public static final String COUNTRY = "country";
+
+    public static final String AGE = "age";
+
+    public static final String COST = "cost";
+
+    public static final String POSITION = "position";
+
+    public static final String COACH = "coach";
+
+    public static final String EXPERIENCE = "experience";
+
+    public static final String SPONSORS = "sponsors";
+
+    public static final String SPONSOR = "sponsor";
+
+    public static final String YEAR = "year";
+
+    public static final String STADIUM = "stadium";
+
+    public static final String CAPACITY = "capacity";
+
+    public static final String CITY = "city";
+
+}

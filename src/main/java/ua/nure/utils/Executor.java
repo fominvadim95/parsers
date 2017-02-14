d13567e2c34fc4b353356f848dd75d7da2215a10
vadym_fomin <vadym.fomin@nure.ua>
2017-02-14 17:38:25 +0200

    Add toString method for entities

--- src/main/java/ua/nure/utils/Executor.java
+++ src/main/java/ua/nure/utils/Executor.java
@@ -1,7 +1,10 @@
 package ua.nure.utils;
 
 
+import ua.nure.entities.Teams;
+import ua.nure.parsers.Marshaller;
 import ua.nure.parsers.Unmarshaller;
+import ua.nure.parsers.dom.DomMarshaller;
 import ua.nure.parsers.dom.DomUnmarshaller;
 
 import java.io.File;
@@ -10,11 +13,17 @@ public class Executor {
 
     private static final String INPUT_FILE = "src/main/resources/teams.xml";
 
+    private static final String OUTPUT_FILE = "src/main/resources/output/dom.xml";
+
     public static void main(String[] args) {
 
         System.out.println("DOM Parser");
         Unmarshaller unmarshaller = new DomUnmarshaller();
-        System.out.println(unmarshaller.unmarshall(new File(INPUT_FILE)));
+        Marshaller marshaller = new DomMarshaller();
+        Teams teams = unmarshaller.unmarshall(new File(INPUT_FILE));
+        System.out.println(teams);
+        marshaller.marshall(teams,new File(OUTPUT_FILE));
+
 
     }
 
